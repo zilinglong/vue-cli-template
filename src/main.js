37ff54ea36970638b25338f42741cb4cf3c4d166
mengmingxiu <mengmingxiu@100tal.com>
2017-11-13 18:19:10 +0800

    添加vuex状态管理器

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import Vuex from 'vuex';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import common from './assets/js/common.js';
@@ -11,6 +12,7 @@ import axios from 'axios';
 Vue.prototype.$axios = axios;
 Vue.use(common);
 Vue.use(VueResource);
+Vue.use(Vuex);
 Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
