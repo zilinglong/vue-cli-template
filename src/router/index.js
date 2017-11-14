309685255d354ebb5c500ee9edfd58138392adf4
mengmingxiu <mengmingxiu@100tal.com>
2017-11-14 17:20:10 +0800

    vue-router添加redirect和懒加载功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,28 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+// import HelloWorld from '@/components/HelloWorld';
 import IndexPage from '@/components/indexPage';
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/IndexPage',
       name: 'IndexPage',
       component: IndexPage
     },
     {
+      path: '/',
+      redirect: '/IndexPage'
+    },
+    {
       path: '/HelloWorld',
       name: 'HelloWorld',
-      component: HelloWorld
+      // component: HelloWorld
+      component: resolve => {
+        require.ensure(['@/components/HelloWorld'], () => {
+          resolve(require('@/components/HelloWorld'));
+        });
+      }
     }
   ]
 });
