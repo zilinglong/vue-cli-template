c97e89405f8e5410b6ecb1cf440fe5eb6e85461f
mengmingxiu <mengmingxiu@100tal.com>
2017-11-20 16:46:47 +0800

    模板html、body、#app添加height为100%，以便容器元素撑满整个屏幕

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,11 @@
 <style>
 @import './assets/css/pub.css';
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'PingFang SC','Microsoft YaHei', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
